c31ea8dd724cf1f2d29fce3a103d9e823cd8fb69
Csmally <1130180692@qq.com>
2024-11-19 23:37:49 +0800

    feat: 按照班级查询学生信息

--- src/routes/student/studentSearch.js
+++ src/routes/student/studentSearch.js
@@ -24,4 +24,22 @@ router.post("/student/search", async (ctx) => {
   }
 });
 
+// 根据学校和班级查询学生信息
+router.post("/student/searchByClass", async (ctx) => {
+  try {
+    const { customerId, classId } = ctx.request.body;
+    const list = await Student.findAndCountAll({
+      // 其他查询条件
+      where: { customerId, classId },
+      order: [["createdAt", "DESC"]], // 排序条件，例如按创建时间倒序
+    });
+    ctx.body = {
+      total: list.count,
+      list: list.rows,
+    };
+  } catch (error) {
+    throw new ErrorObj(error);
+  }
+});
+
 export default router;
