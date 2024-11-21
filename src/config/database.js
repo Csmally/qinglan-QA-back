16715f660c6faad6ee77dd67fe8a668ba9a09aff
Csmally <1130180692@qq.com>
2024-11-21 17:42:20 +0800

    feat: 区分环境数据库

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
 
+const isProd = process.env.NODE_ENV === "production" ? true : false;
 // 配置数据库连接信息
-const sequelize = new Sequelize("ql_db_dev", "root", "cy4u1314", {
+const sequelize = new Sequelize(isProd ? "ql_db_prod" : "ql_db_dev", "root", "cy4u1314", {
   host: "localhost",
   dialect: "mysql",
 });
